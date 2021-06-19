761748a67ec852441356bfea4ea394436ff3103b
Michelle Nguyen <michellenguyen11239@aol.com>
2021-06-19 11:43:13 -0700

    Added client-side routes for login.js, logout.js, and signup.js

--- public/js/logout.js
+++ public/js/logout.js
@@ -1,1 +1,34 @@
 /* JS file handles client side for logging users out  */
+
+const adminLogOut = async () => {
+    const response = await fetch('/api/admin/logout', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert("Failed to log out.");
+    }
+}
+
+const studentLogOut = async () => {
+    const response = await fetch('/api/student/logout', {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      }
+    })
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert("Failed to log out.");
+    }
+}
+
+$("#logOutAdmin1").on('click', adminLogOut);
+$("#logOutAdmin2").on('click', adminLogOut);
+$("#logOutStudent1").on('click', studentLogOut);
+$("#logOutStudent2").on('click', studentLogOut);
