99b5b0bcf13f789f6808ddfb1e1344b6786ab5f8
David Valderrama <dvalderrama08@gmail.com>
2021-06-19 11:13:24 -0700

    Added Course and CourseJunction models

---
+++ controllers/api/course-routes.js
@@ -0,0 +1,5 @@
+const router = require('express').Router();
+const Course = require("../../models/Course");
+const CourseJunction = require("../../models/CourseJunction");
+
+module.exports = router;
--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,8 +1,11 @@
 const router = require('express').Router();
 const adminRoute = require('./admin-routes');
 const studentRoute = require('./student-routes');
+const courseRoute = require('./course-routes');
+const { route } = require('./admin-routes');
 
 router.use('/admin', adminRoute);
 router.use('/student', studentRoute);
+router.use('/courses', courseRoute);
 
 module.exports = router;
