6ec35878ce359c8da426b098174494a4ff5bb84a
Chengyu Dai <aacherythdai@gmail.com>
2021-06-21 13:46:12 -0700

    finish course routes and handlebars for creating course

--- controllers/index.js
+++ controllers/index.js
@@ -1,7 +1,9 @@
 const router = require('express').Router();
 const apiRoutes = require('./api');
 const homeRoutes = require('./home-routes');
+const dashRoutes = require('./dash-routes');
 
+router.use('/dash', dashRoutes);
 router.use('/api', apiRoutes);
 router.use('/', homeRoutes);
 
