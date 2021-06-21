b80fbc486ff477d0a2ef88459e9eb7a5fd5bb807
Michelle Nguyen <michellenguyen11239@aol.com>
2021-06-21 14:53:19 -0700

    Updated route in auth.js and added home-page image to public folder

--- utils/auth.js
+++ utils/auth.js
@@ -1,6 +1,6 @@
 const withAuth = (req, res, next) => { // If the user is not logged in, the user will be redirected to the log in page
     if (!req.session.loggedIn) {
-      res.redirect('/login');
+      res.redirect('/');
     } else {
       next(); // When the user is authenticated
     }
