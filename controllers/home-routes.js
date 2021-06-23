ac6985b0549db273526c802cf73727be675dc1ee
Alex DaRe <alexdare93@gmail.com>
2021-06-22 18:54:29 -0700

    added admin and student dashboards, added routes to main

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,17 @@
 const router = require('express').Router();
 
-// Home routes 
+router.get('/dashboard-admin', (req, res) => {
 
-// Log in page
-router.get('/', (req, res) => {
-    res.render('login', {
+    res.render('dashboard-admin', {
+        loggedIn: req.session.loggedIn,
+        admin: req.session.admin,
+        student: req.session.student
+    });
+})
+
+router.get('/dashboard-student', (req, res) => {
+
+    res.render('dashboard-student', {
         loggedIn: req.session.loggedIn,
         admin: req.session.admin,
         student: req.session.student
