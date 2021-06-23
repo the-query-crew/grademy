8bc124ab53836fdedfc8bb9e1b7c0ea4ae48639e
Michelle Nguyen <michellenguyen11239@aol.com>
2021-06-22 20:44:14 -0700

    Added withAuth util to home-routes

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const withAuth = require('../utils/auth')
 
 // Home routes
 
@@ -12,7 +13,7 @@ router.get('/', (req, res) => {
     });
 })
 
-router.get('/dashboard-admin', (req, res) => {
+router.get('/dashboard-admin', withAuth, (req, res) => {
 
     res.render('dashboard-admin', {
         loggedIn: req.session.loggedIn,
@@ -21,7 +22,7 @@ router.get('/dashboard-admin', (req, res) => {
     });
 })
 
-router.get('/dashboard-student', (req, res) => {
+router.get('/dashboard-student', withAuth, (req, res) => {
 
     res.render('dashboard-student', {
         loggedIn: req.session.loggedIn,
@@ -40,7 +41,7 @@ router.get('/create', (req, res) => {
 })
 
 // Chat page
-router.get('/chat', (req, res) => {
+router.get('/chat', withAuth, (req, res) => {
     res.render('chat',  {
         loggedIn: req.session.loggedIn,
         admin: req.session.admin,
