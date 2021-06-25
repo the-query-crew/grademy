a3d4bab4c7bbd57fa6ad3d5a97378ea5f061389a
Michelle Nguyen <michellenguyen11239@aol.com>
2021-06-24 20:31:12 -0700

    Finished chat functionality, updated style sheet, and updated server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ var io = require('socket.io')(http);
 const sess = {
   secret: 'Super secret secret',
   cookie: { 
-    expires: 600000 // Cookie will expire in 10 minutes
+    expires: 3600000 // Cookie will expire in 1 hour
   },
   resave: false,
   saveUninitialized: true,
@@ -49,24 +49,34 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(routes); 
 
-let users = 0; // Keeps track of number of users
-io.on('connection', (socket) => {
-  console.log('A user joined the room.');
-  users++;
-  console.log(users);
+const users = {}; // Saves all current users 
 
-  socket.on('disconnect', () => {
-    console.log('A user left the room.');
-    users--;
-    console.log(users);
+// Handles server side socket.io on connection
+io.on('connection', (socket) => {
+  socket.on('new-user', (user) => {
+    users[socket.id] = user // Saves the user's socket.id as the key and the user's name as the value
+    socket.broadcast.emit('user-connected', user) // Send to the client the user that connected
+    socket.emit('current-users', users); // Send object of current users when a user connects
+    console.log('A user joined the room.');
   })
 
-socket.on('msg', (data) => {
-  io.sockets.emit('newmsg', data); // Sends to the client side an event called 'newMsgHandler' with the user text
-})
+  socket.on('msg', (data) => {
+    io.sockets.emit('newmsg', data); // Sends to the client side an event called 'newmsg' with the user text
+  })
+
+  socket.on('send-current-users', (values) => {
+    io.sockets.emit('display-current-users', values) // Sends to the client side current users to append to the screen
+  })
+
+  socket.on('disconnect', () => { // When a user disconnects
+    console.log('A user left the room.');
+    socket.broadcast.emit('user-left', users[socket.id]);
+    delete users[socket.id]; // Delete the user that left from the array of objects
+    socket.emit('current-users', users); // Send object of current users when a user disconnects
+  })
 });
 
 // Syncs sequelize
-sequelize.sync({ force: true }).then(() => {
+sequelize.sync({ force: false }).then(() => {
   http.listen(PORT, () => console.log('Now listening'));
 });
