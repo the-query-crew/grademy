9a3544513fd586b810af2af9bba2e2242631b9b7
Alex DaRe <alexdare93@gmail.com>
2021-06-26 11:00:40 -0700

    removed commented out code in dash-routes

--- controllers/dash-routes.js
+++ controllers/dash-routes.js
@@ -10,29 +10,6 @@ router.get('/create', function(req, res) {
         student: req.session.student
     })
 });
-
-// router.get('/dashboard-admin', async (req, res) => {
-//     try {
-//       const courseData = await Course.findAll({
-//         where :
-//             {
-//                 admin_instructor_id: req.session.userID
-//             }
-//         },
-//       );
-  
-//       const courses = courseData.map((course) =>
-//         course.get({ plain: true })
-//       );
-//         console.log(courses)
-//       res.render('dashboard-admin', {
-//         courses,
-//       });
-//     } catch (err) {
-//       console.log(err);
-//       res.status(500).json(err);
-//     }
-//   });
   
 
 module.exports = router;
