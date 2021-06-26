f202422d2ef24873ef5a014bca7c1dac863fc7d2
David Valderrama <dvalderrama08@gmail.com>
2021-06-26 13:19:21 -0700

    Filtered out coures that a student already signed up for from the get all list

--- controllers/courses/course-render-routes.js
+++ controllers/courses/course-render-routes.js
@@ -1,14 +1,24 @@
 const router = require('express').Router();
 const Course = require("../../models/Course");
-const CourseJunction = require("../../models/CourseJunction");
+const Student = require("../../models/Student");
 const withAuth = require('../../utils/auth');
 
 router.get('/signup', withAuth, async (req, res) => {
     try {
 
         const courseData = await Course.findAll({include: {all: true}});
-        const courses = courseData.map(course => course.get({ plain: true }));
-        
+
+        // Filter out those classes that a student is already enrolled in
+        const filteredCourses = courseData.filter(course => {
+            if (course.students.length > 0) {
+                return course.students[0].id !== req.session.userID;
+            }
+
+            return true;
+        });
+
+        const courses = filteredCourses.map(course => course.get({ plain: true }));
+
         res.render('view-all-courses', {
             courses: {courses},
             loggedIn: req.session.loggedIn,
